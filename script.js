5d34934d774f7c90c81bf453cf5c4122b8db7c77
Tamas Matyas <matyas_tommy@hotmail.com>
2024-12-16 09:36:44 -0600

    practice

---
+++ script.js
@@ -0,0 +1,27 @@
+/* function myFunc(argument) {
+  const newObj = JSON.parse(JSON.stringify(obj));
+  newObj.key = "valami";
+
+  return argument;
+}
+
+const obj = {
+  key: "kismacska",
+};
+
+const myArrowFunc = (argument) => argument;
+
+console.log(myFunc(obj));
+console.log(myArrowFunc(obj)); */
+
+// const manipulateText = (string) => {
+//   if (typeof sting !== "string") {
+//     return "please enter a string";
+//   } else {
+//     return "ok";
+//   }
+// };
+
+// console.log(manipulateText(null));
+
+const manipulateText = (string) => {};
